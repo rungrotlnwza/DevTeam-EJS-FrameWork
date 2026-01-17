0957926f3dc79d42e4d45968cfeca74b73ec3454
Rungrot <rungrot.muangchalerm@gmail.com>
2026-01-17 17:44:22 +0700

    fixbug: layouts

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-console.clear();
-require('dotenv').config({quiet: true});
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+console.clear()
+require('dotenv').config({quiet: true})
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const path = require('path')
+const expressLayouts = require('express-ejs-layouts')
+app.set('view engine','ejs')
+app.disable('view cache')
+app.use(expressLayouts)
+app.set('layout',false)
 
 app.get('/',(req,res)=>{
-    res.send('Hello DevTeam EJS Framework');
+    res.render(path.join(__dirname,'./view/index.ejs'))
 })
+
 require('./tools/live_server.js')(app)
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
