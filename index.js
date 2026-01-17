6e1f1574ec1b786c48c6655f490b2052b832c026
Rungrot <rungrot.muangchalerm@gmail.com>
2026-01-17 17:16:43 +0700

    liveserver hello world

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.get('/',(req,res)=>{
     res.send('Hello DevTeam EJS Framework');
 })
-
+require('./tools/live_server.js')(app)
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
 })
---
+++ tools/live_server.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const livereload = require('livereload');
+const expressLivereload = require('connect-livereload');
+// const { log } = require('console');
+
+module.exports = function setupLiveReload(app) {
+    console.log('hello world');
+}
