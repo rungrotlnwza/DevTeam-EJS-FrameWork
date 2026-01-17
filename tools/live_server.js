1f8dcbd446859fc5f3a4e850cd49d7d7d8ba57f9
Rungrot <rungrot.muangchalerm@gmail.com>
2026-01-17 17:19:49 +0700

    feat: livereloade

--- tools/live_server.js
+++ tools/live_server.js
@@ -4,5 +4,14 @@ const expressLivereload = require('connect-livereload');
 // const { log } = require('console');
 
 module.exports = function setupLiveReload(app) {
-    console.log('hello world');
+    // console.log('hello world');
+    if (process.env.LIVERELOAD === 'true') {
+        const Server = livereload.createServer({
+            exts: ['html', 'css', 'js', 'ejs'],
+        });
+        Server.watch([
+            path.join(__dirname, '../')
+        ])
+        app.use(expressLivereload());
+    }
 }
